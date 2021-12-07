49b32fec32fda794fe60ddd95dceebe8590e3d82
john-rocky <john-rocky@hubot.com>
2021-12-08 05:59:28 +0900

    Initial Commit

---
+++ PersonSegmentationSampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonSegmentationSampler
+//
+//  Created by 間嶋大輔 on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
